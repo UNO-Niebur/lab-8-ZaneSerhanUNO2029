05fbe89b04d8278892a9cc8c3f3a7dfe30a32c28
ZaneSerhanUNO2029 <12106466@nebraska.edu>
2026-03-28 18:05:53 +0000

    Completed Lab8

--- ProcessData.py
+++ ProcessData.py
@@ -1,7 +1,7 @@
 #ProcessData.py
-#Name:
-#Date:
-#Assignment:
+#Name: Zane Serhan
+#Date: 3/28/2026
+#Assignment: Lab8 
 
 import random
 
@@ -12,12 +12,44 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
-
-
+  for line in inFile:
+    data = line.split()
+    first = data[0]
+    last = data[1]
+    idNum = data[3]
+    major = data[6]
+    year = data[5]
+    student_id = makeID(first, last, idNum)
+    major_year = majorYear(major, year)
+    output = last + "," + first + "," + student_id + "," + major_year + "\n"
+    outFile.write(output)
+   
 
   #Close files in the end to save and ensure they are not damaged.
   inFile.close()
   outFile.close()
+
+def makeID(first, last, idNum):
+  idLen = len(idNum)
+  while len(last) < 5:
+    last = last + "X"
+  id = first[0] + last + idNum[idLen - 3: ]
+  return id.lower()
+
+def majorYear(major, year):
+  majorCode = major[:3].upper()
+
+  if year == "Freshman":
+    yearCode = "FR"
+  elif year == "Sophomore":
+    yearCode = "SO"
+  elif year == "Junior":
+    yearCode = "JR"
+  elif year == "Senior":
+    yearCode = "SR"
+  
+  return majorCode + "-" + yearCode
+
 
 if __name__ == '__main__':
   main()
